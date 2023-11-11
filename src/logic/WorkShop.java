ed9910ae8ddcb3c32b8be4d988200a31ea246488
Eimyricaurte <eimy.ricaurte01@uptc.edu.co>
2023-11-11 15:03:06 -0500

    Added roman and egolatrous

--- src/logic/WorkShop.java
+++ src/logic/WorkShop.java
@@ -1,24 +1,62 @@
 package logic;
 
 public class WorkShop {
-    public String romanNumber(int num){
-        //TODO Eimy
-        return null;
+    /**
+     * Checks if the given number is equivalent to a roman number between 1 and 3999
+     *
+     * @param num is the integer to be evaluated
+     * @return the roman number equivalent to the integer given, if it's valid
+     */
+    public String romanNumber(int num) {
+        String[] unities = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] thuosandths = {"", "M", "MM", "MMM"};
+
+        int indext, indexh, indextens, indexu;
+
+        indext = num / 1000;
+        num = num % 1000;
+
+        indexh = num / 100;
+        num = num % 100;
+
+        indextens = num / 10;
+        num = num % 10;
+
+        indexu = num;
+
+        return thuosandths[indext] + hundreds[indexh] + tens[indextens] + unities[indexu];
     }
 
-    public String primeFactors(int num){
+    public String primeFactors(int num) {
         //TODO Cesar
         return null;
     }
 
-    public String ownName(String name){
+    public String ownName(String name) {
         //TODO Camila
         return null;
     }
 
+
+    /**
+     * Checks if the given number is egolatrous
+     * @param num is the number to be evaluated
+     * @return true if the number is egolatrous, false if is not
+     */
     public boolean egotisticalNumber(int num){
-        //TODO eimy
-        return false;
+
+        String number = String.valueOf(num);
+        int digits = number.length();
+        int res =0;
+        boolean isEgo = true;
+        for (int index = 0; index< number.length(); index++){
+            int charNum = Character.getNumericValue(number.charAt(index));
+            res += (int)Math.pow(charNum, digits);
+        }
+
+        return res == num ? isEgo : !isEgo;
     }
 
     public boolean magicNumber(int magic){
--- src/view/Runner.java
+++ src/view/Runner.java
@@ -1,36 +1,71 @@
 package view;
 
 import logic.WorkShop;
+import org.w3c.dom.ls.LSOutput;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Runner {
-    static Scanner sc = new Scanner(System.in);
     WorkShop workShop = new WorkShop();
     static Runner runner = new Runner();
     public static void main(String[] args) {
         runner.menu();
     }
     public void menu(){
+        boolean centinel= true;
+            while  (centinel){
+                int option = Integer.parseInt(JOptionPane.showInputDialog("********* MENU ******** \n" +
+                        "1. Roman numeral \n" +
+                        "2. Prime factors\n" +
+                        "3. Own Name\n" +
+                        "4. Egotistical numbers \n" +
+                        "5. Magic number\n" +
+                        "6. Friendly numbers\n" +
+                        "7. Go out"));
+                switch (option){
+                    case 1 -> JOptionPane.showInputDialog(roman() +
+                            "\n Please press enter to go back to menu");
+                    case 2 -> workShop.primeFactors(10);
+                    case 3 -> workShop.ownName(null);
+                    case 4 -> JOptionPane.showInputDialog(egoNumber() +
+                            "\n Please press enter to go back to menu");
+                    case 5 -> workShop.magicNumber(10);
+                    case 6 -> workShop.friendlyNumbers(10,20);
+                    case 7 -> centinel = false;
+                    default -> System.err.println("Invalid Option");
+                }
+            }
+    }
 
-        System.out.println("********* MENU ******** \n" +
-                "1. Roman numeral \n" +
-                "2. Prime factors\n" +
-                "3. Own Name\n" +
-                "4. Egotistical numbers" +
-                "5. Magic number\n" +
-                "6. Friendly numbers" +
-                "7. Go out" );
-        int option = sc.nextInt();
-        switch (option){
-            case 1 -> workShop.romanNumber(10);
-            case 2 -> workShop.primeFactors(10);
-            case 3 -> workShop.ownName(null);
-            case 4 -> workShop.egotisticalNumber(10);
-            case 5 -> workShop.magicNumber(10);
-            case 6 -> workShop.friendlyNumbers(10,20);
-            case 7 -> runner.menu();
-            default -> System.err.println("Invalid Option");
+    /**
+     * Receives the number to be evaluated
+     * @return the equivalent roman number
+     */
+    public String roman() {
+        int num = 0;
+        try{
+            num = Integer.parseInt(JOptionPane.showInputDialog("Please enter a number between 1 and 3999"));
+            return workShop.romanNumber(num);
+        }catch (Exception e){
+            return "Please the next time enter a number \n";
         }
+
+
+    }
+
+    /**
+     * Receives a number to evaluate if it is egolatrous or not
+     * @return true if the number is egolatrous
+     */
+    private boolean egoNumber() {
+        int number=0;
+        try{
+            number = Integer.parseInt(JOptionPane.showInputDialog("Please enter a number"));
+            return workShop.egotisticalNumber(number);
+        }catch (Exception e){
+            return false;
+        }
+
     }
 }
